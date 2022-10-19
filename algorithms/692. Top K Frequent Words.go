49db3e2e12fb1a4f004fa064670312a8bcf95044
RUI <a6504130@gmail.com>
2022-10-19 10:03:31 +0900

    update

--- algorithms/692. Top K Frequent Words.go
+++ algorithms/692. Top K Frequent Words.go
@@ -1,1 +1,21 @@
 package algorithms
+
+import "sort"
+
+func topKFrequent(words []string, k int) []string {
+	wordCount := make(map[string]int)
+	for _, word := range words {
+		wordCount[word]++
+	}
+	wordList := make([]string, 0, len(wordCount))
+	for word := range wordCount {
+		wordList = append(wordList, word)
+	}
+	sort.Slice(wordList, func(i, j int) bool {
+		if wordCount[wordList[i]] == wordCount[wordList[j]] {
+			return wordList[i] < wordList[j]
+		}
+		return wordCount[wordList[i]] > wordCount[wordList[j]]
+	})
+	return wordList[:k]
+}
