c7028ae91e99dee3ca6141ddf930a066e5994394
RUI <a6504130@gmail.com>
2022-10-21 04:42:40 +0900

    update

---
+++ algorithms/1. Two Sum.go
@@ -0,0 +1,12 @@
+package algorithms
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, num := range nums {
+		if j, ok := m[target-num]; ok {
+			return []int{j, i}
+		}
+		m[num] = i
+	}
+	return nil
+}
