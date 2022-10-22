11047841f1b382a08d36935d832c470512602025
RUI <a6504130@gmail.com>
2022-10-22 17:06:34 +0900

    update

--- algorithms/1832. Check if the Sentence Is Pangram.go
+++ algorithms/1832. Check if the Sentence Is Pangram.go
@@ -1,20 +1,5 @@
 package main
 
-/*
-pangram 是英文字母中的每个字母至少出现一次的句子。
-给定一个仅包含小写英文字母的字符串句子，如果句子是 pangram，则返回 true，否则返回 false。
-
-Example 1:
-
-Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
-Output: true
-Explanation: 句子至少包含英文字母表中的每个字母
-Example 2:
-
-Input: sentence = "leetcode"
-Output: false
-*/
-
 func checkIfPangram(sentence string) bool {
 	var m = make(map[rune]bool)
 	for _, c := range sentence {
--- algorithms/76. Minimum Window Substring.go
+++ algorithms/76. Minimum Window Substring.go
@@ -1,32 +1,5 @@
 package main
 
-/*
-Given two strings and of lengths and respectively, return the minimum window substring of such that every character in (including duplicates) is included in the window. If there is no such substring, return the empty string .stmnst""
-
-The testcases will be generated such that the answer is unique.
-
-A substring is a contiguous sequence of characters within the string.
-
-
-
-Example 1:
-
-Input: s = "ADOBECODEBANC", t = "ABC"
-Output: "BANC"
-Explanation: The minimum window substring "BANC" includes 'A', 'B', and 'C' from string t.
-Example 2:
-
-Input: s = "a", t = "a"
-Output: "a"
-Explanation: The entire string s is the minimum window.
-Example 3:
-
-Input: s = "a", t = "aa"
-Output: ""
-Explanation: Both 'a's from t must be included in the window.
-Since the largest window of s only has one 'a', return empty string.
-*/
-
 func minWindow(s string, t string) string {
 	if len(s) < len(t) {
 		return ""
